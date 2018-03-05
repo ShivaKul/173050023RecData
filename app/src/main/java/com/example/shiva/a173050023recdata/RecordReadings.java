9394a05f77f9429238b012f3291d0933f86d45e5
173050023 <shivakul@cse.iitb.ac.in>
2018-03-05 14:11:09 +0530

    Added Persistence to Settings and Login

---
+++ app/src/main/java/com/example/shiva/a173050023recdata/RecordReadings.java
@@ -0,0 +1,53 @@
+package com.example.shiva.a173050023recdata;
+
+import android.app.Service;
+import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.AsyncTask;
+import android.os.IBinder;
+import android.util.Log;
+
+public class RecordReadings extends Service implements SensorEventListener {
+    private static final String DEBUG_TAG = "RecordReadingsService";
+
+    private SensorManager sensorManager = null;
+    private Sensor sensor = null;
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_NORMAL);
+        return START_STICKY;
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        return null;
+    }
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        // grab the values and timestamp -- off the main thread
+        new SensorEventLoggerTask().execute(event);
+        // stop the service
+        //stopSelf();
+    }
+
+    private class SensorEventLoggerTask extends AsyncTask<SensorEvent, Void, Void> {
+        @Override
+        protected Void doInBackground(SensorEvent... events) {
+            SensorEvent event = events[0];
+            // log the value
+            Log.v("Service", "Hello");
+            return null;
+        }
+    }
+}
